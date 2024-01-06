55cda9001d04889792bd66ec5830990a2de3368d
pankajsubha <155792475+pankajsubha@users.noreply.github.com>
2024-01-06 09:47:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -38,7 +38,7 @@ DB_NAME = environ.get("DB_NAME", "sp_filestore")
 # Bot Information
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 BOT_USERNAME = environ.get("BOT_USERNAME", "") # your bot username without @
-PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
+PICS = (environ.get('PICS', 'https://te.legra.ph/file/5d045f2377e657a11496e.jpg https://te.legra.ph/file/5d045f2377e657a11496e.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
 AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
